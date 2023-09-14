243eea86d46432f612d0e1db5fe427fc4519efb2
Renzo Yesquén <ryesquen@gmail.com>
2023-09-13 22:05:48 -0500

    footer17

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const close_menu = document.querySelector('.close_menu')
 const open_menu = document.querySelector('.open_menu')
 
 open_menu.addEventListener('click', () => {
-  links_menu.classList.remove('no_show')
   links_menu.classList.add('show')
+  links_menu.classList.remove('no_show')
 })
 
 close_menu.addEventListener('click', () => {
-  links_menu.classList.remove('show')
   links_menu.classList.add('no_show')
+  links_menu.classList.remove('show')
 })
